a38ff46561c028889539c002f6c764bb7b08e4a2
Gentil <90131555+LiZongGentil@users.noreply.github.com>
2022-05-17 20:27:55 +0800

    Add files via upload

---
+++ router/router.go
@@ -0,0 +1,37 @@
+/*
+路由表：api调用接口，绑定接口与对应处理函数，设置视频读取位置为/static下
+router: connect api interface and the target func, make the net path "/static" in the real path "./public"
+*/
+
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/xcl-zyl/douyin/controller"
+)
+
+func InitRouter(r *gin.Engine) {
+	// public directory is used to serve static resources
+	r.Static("/static", "./public")
+
+	apiRouter := r.Group("/douyin")
+
+	// basic apis
+	apiRouter.GET("/feed/", controller.Feed)
+	apiRouter.GET("/user/", controller.UserInfo)
+	apiRouter.POST("/user/register/", controller.Register)
+	apiRouter.POST("/user/login/", controller.Login)
+	apiRouter.POST("/publish/action/", controller.Publish)
+	apiRouter.GET("/publish/list/", controller.PublishList)
+
+	// extra apis - I
+	apiRouter.POST("/favorite/action/", controller.FavoriteAction)
+	apiRouter.GET("/favorite/list/", controller.FavoriteList)
+	apiRouter.POST("/comment/action/", controller.CommentAction)
+	apiRouter.GET("/comment/list/", controller.CommentList)
+
+	// extra apis - II
+	apiRouter.POST("/relation/action/", controller.RelationAction)
+	apiRouter.GET("/relation/follow/list/", controller.FollowList)
+	apiRouter.GET("/relation/follower/list/", controller.FollowerList)
+}
